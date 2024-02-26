0e9a34aefa5d01b127ebc12af84dd0c3bb08c2d6
root <root@ip-172-31-1-89.ap-south-1.compute.internal>
2024-02-26 16:49:48 +0000

    added

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.10.0"
+	} 
+  }
+}
+provider "aws" {
+  region = "ap-south-1"
+  }
+resource "aws_instance" "MyWk20Project" {
+  ami = "ami-0e670eb768a5fc3d4"
+  instance_type = "t2.micro"
